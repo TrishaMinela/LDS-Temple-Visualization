9fb14ccb312117edc94f13fc1db05727d22af074
Litian Zhang <litian_zhang17@163.com>
2020-08-24 17:57:57 -1000

    Fix bug: when app first launches and rotate before click on any temple, app would crash. because we are updating dialog layout and there is no dialog present yet, now bug is fixed

--- app/src/main/java/edu/byuh/cis/templevis/TempleView.java
+++ app/src/main/java/edu/byuh/cis/templevis/TempleView.java
@@ -622,23 +622,23 @@ public class TempleView extends View {
         //singleTempleImageView.updatePositionAndSizeOnceOrientationChanged();
         if (singleTempleImageView != null) { // the rotate phone without clicking on a temple
             singleTempleImageView.orientationJustChanged(b);
-        }
-        //singleTempleImageView.invalidate();
+            //singleTempleImageView.invalidate();
 
-        // reset single temple dialog size according to screen size once orientation change happens
-        WindowManager.LayoutParams params = singleTempleDialog.getWindow().getAttributes();
-        int h = 0;
-        int w = 0;
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
-            h = (int)(Math.min(windowHeight, windowWidth) * 1.2);
-            w = (int)Math.min(windowHeight, windowWidth);
-        } else {
-            h = (int)(Math.min(windowHeight, windowWidth) * 0.9);
-            w = (int)Math.min(windowHeight, windowWidth);
+            // reset single temple dialog size according to screen size once orientation change happens
+            WindowManager.LayoutParams params = singleTempleDialog.getWindow().getAttributes();
+            int h = 0;
+            int w = 0;
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+                h = (int)(Math.min(windowHeight, windowWidth) * 1.2);
+                w = (int)Math.min(windowHeight, windowWidth);
+            } else {
+                h = (int)(Math.min(windowHeight, windowWidth) * 0.9);
+                w = (int)Math.min(windowHeight, windowWidth);
+            }
+            params.height = h;
+            params.width =  w;
+            singleTempleDialog.getWindow().setAttributes(params);
         }
-        params.height = h;
-        params.width =  w;
-        singleTempleDialog.getWindow().setAttributes(params);
     }
 
     public void getWindowSize(float w, float h) {
